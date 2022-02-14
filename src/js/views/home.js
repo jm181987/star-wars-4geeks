49b39d2ef09210f0e9d1d922258f27e9c7c4200a
aseg3092 <gabrielsalazar3092@gmail.com>
2022-02-14 23:28:03 +0000

    agregando a Github

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -3,13 +3,12 @@ import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
 
 export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
+	<div >
 		<p>
-			<img src={rigoImage} />
+			Characters
 		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
+		<p>
+			Planets
+		</p>
 	</div>
 );
