cd649c839340262ab549cf6fe6afadf27d7c2639
jm181987 <jorgitom18@gmail.com>
2022-12-22 22:20:23 +0000

    actualizando

--- src/js/component/footer.js
+++ src/js/component/footer.js
@@ -4,7 +4,7 @@ export const Footer = () => (
 	<footer className="footer mt-auto py-3 text-center">
 		<p>
 			<i className="fas fa-heart text-danger" /> by{" "}
-			<a href="https://github.com/AstridMata?tab=repositories" target="_blank">Astrid</a>
+			<a href="https://github.com/jm181987?tab=repositories" target="_blank">JORGE MARQUEZ</a>
 		</p>
 	</footer>
 );
