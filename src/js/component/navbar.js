5023d01d422add10d1b453261a29491f40c9dc26
jm181987 <jorgitom18@gmail.com>
2022-12-22 23:45:49 +0000

    actualizacion para entrega

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -5,13 +5,16 @@ import { Context } from "../store/appContext";
 export const Navbar = () => {
   const { store, actions } = useContext(Context);
   return (
+    
     <nav className="navbar navbar-light bg-light mb-3 container">
       <Link to="/">
         <img
           style={{ objectFit: "cover" }}
           className="navbar-brand mb-0 h1"
           src="https://pngimg.com/uploads/starwars/starwars_PNG21.png" />
+          
       </Link>
+      <iframe  width="100" height="100" src="https://www.youtube.com/embed/-bzWSJG93P8" title="Star Wars- The Imperial March (Darth Vader's Theme)" frameborder="0" allow="autoplay" allowfullscreen> clic para un fondo musical acorde</iframe>
       <div className="ml-auto">
         <Link to="/">
           <div className="nav-item dropdown">
--- src/js/layout.js
+++ src/js/layout.js
@@ -38,6 +38,7 @@ const Layout = () => {
 				</ScrollToTop>
 			</BrowserRouter>
 		</div>
+		
 	);
 };
 
