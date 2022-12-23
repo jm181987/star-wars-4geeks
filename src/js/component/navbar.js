f41584c9c44840627e15285f1c8ea8a22c804b8c
jm181987 <jorgitom18@gmail.com>
2022-12-23 00:00:59 +0000

    entrega

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -6,23 +6,23 @@ export const Navbar = () => {
   const { store, actions } = useContext(Context);
   return (
     
-    <nav className="navbar navbar-light bg-light mb-3 container">
+    <nav className="navbar navbar-light  mb-3 container">
       <Link to="/">
         <img
           style={{ objectFit: "cover" }}
-          className="navbar-brand mb-0 h1"
+          className="navbar-brand mb-3 h1"
           src="https://pngimg.com/uploads/starwars/starwars_PNG21.png" />
           
       </Link>
-      <iframe  width="100" height="100" src="https://www.youtube.com/embed/-bzWSJG93P8" title="Star Wars- The Imperial March (Darth Vader's Theme)" frameborder="0" allow="autoplay" allowfullscreen> clic para un fondo musical acorde</iframe>
+      <iframe className="youtube mb-3" width="100" height="100" src="https://www.youtube.com/embed/-bzWSJG93P8" title="Star Wars- The Imperial March (Darth Vader's Theme)" frameborder="0" allow="autoplay" allowfullscreen> clic para un fondo musical acorde</iframe>
       <div className="ml-auto">
         <Link to="/">
           <div className="nav-item dropdown">
-            <span className="nav-link dropdown-toggle bg-secondary text-white" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            <span className="nav-link mb-3 overflow-visible dropdown-toggle bg-secondary text-white" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
               Favorites {store.favoritos.length}</span>
             <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
               {store.favoritos.map((item) => {
-                return <li><a className="dropdown-item"> <span onClick={() => { actions.eliminaFavorito(item.id) }} className={"fa fa-trash"}>&nbsp; Delete this favourite &nbsp;</span>{item.name}</a></li>
+                return <li><a className="dropdown-item"> <span onClick={() => { actions.eliminaFavorito(item.id) }} className={"fa fa-trash"}></span>{item.name}</a></li>
               })}
             </ul></div>
         </Link>
